71acecf45bd8b56bf94e28cf4fdbc6a772644183
shettysanath4u <53611873+shettysanath4u@users.noreply.github.com>
2024-06-27 15:09:59 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,5 +1,22 @@
 #include "StringCalculator.h"
 
+vector<int> get_numbers(const string &input) {
+	regex numbers_only("(-?\\d+)+");
+
+	vector<int> numbers;
+	for_each(sregex_token_iterator(input.begin(), input.end(), numbers_only),
+				sregex_token_iterator(),
+				[&numbers](string s) { add_if_valid(numbers, s); });
+
+	return numbers;
+}
 int StringCalculator::add(string input){
-        return 0;
+        if (input.empty())
+		return 0;
+
+	vector<int> numbers = get_numbers(input);
+
+	check_for_negatives(numbers);
+
+	return 0;
 }
