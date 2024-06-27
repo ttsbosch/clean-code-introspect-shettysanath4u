036f8e9b371de36d84060bb0bba18eb102786de8
shettysanath4u <53611873+shettysanath4u@users.noreply.github.com>
2024-06-27 16:12:23 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -4,6 +4,32 @@
 #include <string>
 #include <regex>
 
+class GetNegatives {
+private:
+	string negatives;
+
+public:
+	void operator()(int i) {
+		if (i < 0)
+			negatives.append(to_string(i) + ",");
+	}
+
+	operator string() {
+		if (negatives.length() > 0)
+			negatives.pop_back();
+
+		return negatives;
+	}
+};
+
+void check_for_negatives(const vector<int> &numbers) {
+	string negatives = for_each(numbers.begin(), numbers.end(), GetNegatives());
+
+	if (negatives.size() > 0) {
+		throw invalid_argument("Negatives not allowed: " + negatives);
+	}
+}
+
 void add_if_valid(vector<int> &numbers, string number_string) {
 	int number = stoi(number_string);
 	if (number <= 1000)
